f43cd449e8f197c2b85408bb4375a202b7eeea83
TrisiaIntan <intantrisia001@gmail.com>
2021-06-21 05:55:38 +0700

    Initial commit

---
+++ app/src/main/java/com/project/doa/model/ModelDoa.kt
@@ -0,0 +1,9 @@
+package com.project.doa.model
+
+
+class ModelDoa {
+    var strId: String? = null
+    var strTitle: String? = null
+    var strArabic: String? = null
+    var strTranslation: String? = null
+}
